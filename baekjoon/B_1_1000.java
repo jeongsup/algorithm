29c170b5106ad595f52cdeacd1111fc4f435ee25
ggobugi <ninjaggobugi@naver.com>
2021-02-20 12:05:45 +0900

    입출력과 사칙연산 알고리즘 업로드

---
+++ baekjoon/B_1_1000.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class B_1_1000 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a, b, c;
+        a = scan.nextInt();
+        b = scan.nextInt();
+        c = scan.nextInt();
+
+        System.out.println((a + b) % c);
+        System.out.println(((a % c) + (b % c)) % c);
+        System.out.println((a * b) % c);
+        System.out.println(((a % c) * (b % c)) % c);
+    }
+}
